fb731aeb40af570a068cad0bc09f7ec8af8db286
John Zhen <john.zhen24@myhunter.cuny.edu>
2020-11-22 15:44:54 -0500

    refactored weight() func and added another test

--- Field.cpp
+++ Field.cpp
@@ -7,7 +7,6 @@ void Field::computePathCost(const std::vector<std::vector<int>> &v)
     else
     {
         dpPathCost[0][0] = v[0][0];
-
         for(int i = 1; i < v.size(); i++)
         {
             dpPathCost[i][0] = dpPathCost[i-1][0] + v[i][0];
@@ -63,75 +62,17 @@ Field::Field(std::vector<std::vector<int>> && rhs)
 
 int Field::Weight(int x1, int y1, int x2, int y2) const
 {
-    if(dp.empty()) return 0;
     bool outOfBounds = x1 < 0 || y1 < 0  || x2 < 0 || y2 < 0 || x1 >= dp[0].size() || x2 >= dp[0].size() || y1 >= dp.size() || y2 >= dp.size();
     if(outOfBounds) throw std::out_of_range("out of bounds");
-    int startx = 0;
-    int endx = 0;
-    int starty = 0;
-    int endy = 0;
-
-    startx = std::min(x1,x2);
-    endx = std::max(x1,x2);
-    starty = std::min(y1,y2);
-    endy = std::max(y1,y2);
-    //y in dp is x
-    //x in dp is y
-    bool samePointAtTop = startx == endx && starty == 0 && endy == 0; // same point at the outer most part of the matrix
-    bool samePoint = startx == endx && starty == endy; // same point not at border of matrix
-    bool horizontalLine = starty == endy && endy > 0; //points form a horizontal line
-    bool horizontalLineAtTop = endy == 0 && starty == endy;
-    bool verticalLine = startx == endx && endx > 0;
-    bool verticalLineAtLeft = endx == 0 && startx == 0;
-    bool topLeftSquare = startx == 0 && starty == 0;
-    bool leftSquare = startx == 0 && endx >= 1;
-    bool topRightSquare = endx == dp[0].size()-1 && starty == 0;
-    bool bottomRightSquare = endy == dp.size()-1 && endx == dp[0].size()-1;
-    bool middleSquare = startx >= 1 && endx < dp[0].size()-1;
-    if(samePointAtTop)
-    {
-        if(startx > 0)
-        {
-            return dp[endy][endx] - dp[endy][endx-1];
-        }
-        return dp[endy][endx];
-    }
-    if(samePoint)
-    {
-        if(endx == 0)
-        {
-            return dp[starty][startx]-dp[starty-1][startx];
-        }
-        return dp[starty][startx]-dp[starty-1][startx] - dp[starty][startx-1] + dp[starty-1][startx-1]; // same point
-    }
-    if(horizontalLine)
-    {
-        if(startx == 0)
-        {
-            return dp[endy][endx]-dp[endy-1][endx];
-        }
-        return dp[endy][endx] - dp[endy-1][endx] - dp[starty][startx-1] + dp[starty-1][startx-1]; // horizontal line
-    }
-    if(horizontalLineAtTop)
-    {
-        if(startx == 0)
-        {
-            return dp[endy][endx];
-        }
-        return dp[endy][endx]-dp[endy][startx-1]; // horizontal line but at the top
-    }
-    if(verticalLineAtLeft || topLeftSquare) return dp[endy][endx];
-    if(verticalLine) return dp[endy][endx] - dp[endy][endx-1]; // vertical line
-    if(topRightSquare) return dp[endy][endx]-dp[endy][startx-1];
-    if(bottomRightSquare || middleSquare)
-    {
-        if(startx == 0) return dp[endy][endx]-dp[starty-1][endx];
-        else if(starty == 0) return dp[endy][endx] - dp[endy][startx-1];
-        return dp[endy][endx] - dp[endy][startx-1] - dp[starty-1][endx] + dp[starty-1][startx-1];
-    }
-    if(leftSquare) return dp[endy][endx] - dp[starty-1][endx];
-    return dp[endy][endx] - dp[starty+1][startx-1] - dp[starty-1][startx+1] + dp[starty-1][startx-1];
-
+    int startx = std::min(x1,x2);
+    int endx = std::max(x1,x2);
+    int starty = std::min(y1,y2);
+    int endy = std::max(y1,y2);
+    int curr = dp[endy][endx];
+    if(startx > 0) curr -= dp[endy][startx-1];
+    if(starty > 0) curr -= dp[starty-1][endx];
+    if(startx > 0 && starty > 0) curr+=dp[starty-1][startx-1];
+    return curr;
 }
 
 int Field::PathCost() const
